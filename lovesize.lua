9a399ddd90952d557ec44044cb08ece8c24b374a
Ricardo Bustamante <ricardobqueiroz@gmail.com>
2018-04-21 09:33:39 -0300

    Fix issues regarding scale value being used in the translation calculation. Fix swapped width/height variables. Fix variable "lovesize.height" being used instead of lh (from the test implementation).

--- lovesize.lua
+++ lovesize.lua
@@ -1,5 +1,5 @@
 --[[
-    lovesize 0.3 by RicardoBusta
+    lovesize 0.4 by RicardoBusta
     https://github.com/RicardoBusta/
     https://github.com/RicardoBusta/lovesize
 
@@ -7,19 +7,20 @@
 ]]
 local lovesize = {}
 local lw, lh = 800, 600 -- lovesize target size
-local lx, ly = 0 -- translation required to center the game area
+local lx, ly = 0, 0 -- translation required to center the game area
 local ls = 1 -- scale to fit the game screen to love screen
 
 -- Set lovesize with the desired width, height and options
 function lovesize.set(w, h, options)
     lw = w or lw
-    lovesize.height = h or lovesize.height
+    lh = h or lh
     lovesize.resize(love.graphics.getWidth(), love.graphics.getHeight())
 end
 
 -- Used to start the lovesize application
 function lovesize.begin()
     -- Scissors will keep the game from rendering outside the specified screen. Clean the letterbox before this call.
+    love.graphics.push()
     love.graphics.setScissor(lx, ly, love.graphics.getWidth() - 2 * lx, love.graphics.getHeight() - 2 * ly)
     love.graphics.translate(lx, ly)
     love.graphics.scale(ls, ls)
@@ -28,18 +29,21 @@ end
 -- Used to finish the lovesize application
 function lovesize.finish()
     love.graphics.setScissor()
+    love.graphics.pop()
 end
 
 -- Used to calculate the values used by lovesize
 function lovesize.resize(width, height)
     local sx = width / lw
-    local sy = height / lovesize.height
+    local sy = height / lh
 
     -- Check which size scales the most, and add letterbox space to it
     if sx > sy then
-        ls, lx, ly = sy, (width - lw * ls) / 2, 0
+        ls = sy 
+        lx, ly = (width - lw * ls) / 2, 0
     else
-        ls, lx, ly = sx, 0, (height - lovesize.height * ls) / 2
+        ls = sx
+        lx, ly = 0, (height - lh * ls) / 2
     end
 end
 
@@ -51,7 +55,7 @@ end
 
 -- Checks if the coordinate is inside the screen
 function lovesize.inside(x, y)
-    return x >= lx and x < love.graphics.getWidth() - lx and y >= ly and y < love.graphics.getWidth() - ly
+    return x >= lx and x < love.graphics.getWidth() - lx and y >= ly and y < love.graphics.getHeight() - ly
 end
 
 function lovesize.getWidth()
