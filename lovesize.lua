a66121f8af436fe8cf0c387cec75eb7669e17410
Ricardo Bustamante <ricardobqueiroz@gmail.com>
2018-04-22 00:28:24 -0300

    reduce the code a little bit

--- lovesize.lua
+++ lovesize.lua
@@ -1,5 +1,5 @@
 --[[
-    lovesize 0.4 by RicardoBusta
+    lovesize 0.5 by RicardoBusta
     https://github.com/RicardoBusta/
     https://github.com/RicardoBusta/lovesize
 
@@ -9,27 +9,28 @@ local lovesize = {}
 local lw, lh = 800, 600 -- lovesize target size
 local lx, ly = 0, 0 -- translation required to center the game area
 local ls = 1 -- scale to fit the game screen to love screen
+local lgr = love.graphics
 
 -- Set lovesize with the desired width, height and options
 function lovesize.set(w, h, options)
     lw = w or lw
     lh = h or lh
-    lovesize.resize(love.graphics.getWidth(), love.graphics.getHeight())
+    lovesize.resize(lgr.getWidth(), lgr.getHeight())
 end
 
 -- Used to start the lovesize application
 function lovesize.begin()
     -- Scissors will keep the game from rendering outside the specified screen. Clean the letterbox before this call.
-    love.graphics.push()
-    love.graphics.setScissor(lx, ly, love.graphics.getWidth() - 2 * lx, love.graphics.getHeight() - 2 * ly)
-    love.graphics.translate(lx, ly)
-    love.graphics.scale(ls, ls)
+    lgr.push()
+    lgr.setScissor(lx, ly, lgr.getWidth() - 2 * lx, lgr.getHeight() - 2 * ly)
+    lgr.translate(lx, ly)
+    lgr.scale(ls, ls)
 end
 
 -- Used to finish the lovesize application
 function lovesize.finish()
-    love.graphics.setScissor()
-    love.graphics.pop()
+    lgr.setScissor()
+    lgr.pop()
 end
 
 -- Used to calculate the values used by lovesize
@@ -55,7 +56,7 @@ end
 
 -- Checks if the coordinate is inside the screen
 function lovesize.inside(x, y)
-    return x >= lx and x < love.graphics.getWidth() - lx and y >= ly and y < love.graphics.getHeight() - ly
+    return x >= lx and x < lgr.getWidth() - lx and y >= ly and y < lgr.getHeight() - ly
 end
 
 function lovesize.getWidth()
